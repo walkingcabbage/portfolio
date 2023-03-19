026cea0a137982913b9e4f5a7a47cde2b822024f
walkingcabbage <aksdn1513@gmail.com>
2023-03-19 21:32:40 +0900

    프로젝트 섹션 수정

--- src/components/ProjectCon.vue
+++ src/components/ProjectCon.vue
@@ -1,241 +1,92 @@
 <template>
-  <div class="project-wrap">
-    <div class="project-sec sec1">
-        <div class="project-left">
-          <div class="title-wrap">
-            <h2>PROJECT</h2>
-          </div>
-          <div class="title-wrap clone">
-            <h2>WEBCLONE</h2>
-          </div>
-          <div class="site-logo-wrap">
-            <img class="site-logo" :src="logoUrl" alt="">
-            <!-- ../assets/imgs/KWAlogo.png -->
-          </div>
-        </div>
-        <div class="project-right">
-          <div class="project-right-top">
-            <span class="pagenation">{{current}}/{{total}}</span>
-            <div class="project-title">
-              <h3>{{title}}</h3>
-            </div>
-          </div>
-          <div class="project-right-bottom">
-            <p class="disc">{{disc}}</p>
-            <div class="details">
-              <h4>개발기간</h4>
-              <p>{{devtime}}</p>
-            </div>
-            <div class="details">
-              <h4>직무</h4>
-            <p>{{job}}</p>
-            </div>
-            <div class="details">
-              <h4>기여도</h4>
-            <p>{{contr}}</p>
-            </div>
-            <div class="details">
-              <h4>스택</h4>
-            <p>{{stack}}</p>
-            </div>
-            <div class="details">
-              <h4>기타</h4>
-            <a target="_blank" :href='githubUrl'>Github</a>
-            <a v-if="demoUrl" target="_blank" :href='demoUrl'>Demo</a>
-            </div>
-          </div>
-        </div>
+  <div class="project-con-wrapper">
+    <div class="img-wrap">
+      <img src="@/assets/imgs/kwa-tit.png" alt="">
+    </div>
+    <div class="con-wrap">
+      <div class="disc-wrap">
+        <h3>한국 폐기물 협회</h3>
+        <p>HTML, CSS, JS, Node.js, Express.js, MySQL</p>
       </div>
+      <div class="btns-wrap">
+        <button><img src="@/assets/imgs/iconmonstr-home-6-48.png" alt=""></button>
+        <button><img src="@/assets/imgs/iconmonstr-github-1-48.png" alt=""></button>
+        <button><img src="@/assets/imgs/iconmonstr-copy-filled-48.png" alt=""></button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import { onMounted } from "vue";
-export default {
-  props:{
-    logoUrl:String,
-    title:String,
-    disc:String,
-    devtime:String,
-    job:String,
-    contr:String,
-    stack:String,
-    githubUrl:String,
-    demoUrl:String,
-    total:Number,
-    current:Number,
-  },
-  setup(){
-    onMounted(()=>{
-    })
-  }
-}
-</script>
-<style scoped>
-  .project-wrap{
-    width: 100%;
-    height: 100%;
-    color: #fff;
-  }
-  .project-sec{
-    display: flex;
-    height: 100%;
-    width: 100%;
-  }
-  .project-left{
-    width: 45%;
-    border-right: 1px solid #fff;
-  }
-  .project-right{
-    width: 55%;
-    border-right: 1px solid #fff;
-  }
-  .project-right-top{
-    position: relative;
-    border-bottom: 1px solid #fff;
-  }
-  .title-wrap{
-    padding-left: 15.625%;
-    padding-top: 100px;
-  }
-  .project-title{
-    font-size: 48px;
-    font-weight: bold;
-    padding-top: 100px;
-    padding-bottom: 40px;
-    text-align: center;
-  }
-  .pagenation{
-    position: absolute;
-    top: 10px;
-    right: 10px;
-  }
-  .project-right-bottom{
-    padding: 70px 15.625%;
-    /* height: 100%; */
-    font-family: 'Noto Sans KR', sans-serif;
-  }
-  .site-logo-wrap{
-    text-align: center;
-    padding-top: 200px;
-  }
-  .site-logo{
-    width: 60%;
-  }
-  .disc{
-    margin-bottom: 40px;
-    line-height: 20px;
-  }
-  .details{
-    margin-bottom: 20px;
+  export default {
 
   }
-  .details>a:nth-child(3){
-    padding-left: 10px;
+</script>
+<style lang="css">
+  .project-con-wrapper {
+    width: calc(25% - 20px);
+    box-shadow: 0px 0px 6px gray;
+    padding: 30px 10px;
+    padding-top: 10px;
+    border-radius: 5px;
+    transition: all 0.3s;
   }
-    .details:last-child{
-    overflow:hidden;
-      text-overflow:ellipsis;
-      white-space:nowrap;
-    }
-  .details>h4{
-    color: #FF9128;
+
+  .img-wrap {
+    width: 100%;
+    margin-bottom: 20px;
+  }
+
+  .img-wrap img {
+    width: 100%;
+  }
+  .con-wrap{
+    text-align: center;
+  }
+  .disc-wrap {
+    text-align: center;
+  }
+
+  .disc-wrap h3 {
     font-size: 20px;
     font-weight: bold;
-    margin-bottom: 15px;
-  }
-  .details>a{
-    color: #fff;
-    text-decoration: underline;
-  }
-  .swiper-slide:nth-child(4) .title-wrap,.swiper-slide:nth-child(5) .title-wrap{
-    display: none;
-  }
-  .clone{
-    display: none;
-    padding-left: 15.625%;
-    padding-top: 100px;
-  }
-  .swiper-slide:nth-child(4) .clone,.swiper-slide:nth-child(5) .clone{
-    display: block;
-  }
-  .details>p,.details>a{
-    line-height: 20px;
-  }
-  @media (max-width:1400px) {
-    .project-sec{
-      flex-direction: column;
-      border-right: 0px solid #fff;
-    }
-    .title-wrap{
-      padding-left: 10%;
-    }
-    .project-left{
-      width: 100%;
-      padding-bottom: 40px;
-      border-bottom: 1px solid #fff;
-      border-right: 0;
-    }
-    .project-right{
-      width: 100%;
-    }
-    .site-logo-wrap{
-      padding-top: 50px;
-      padding-left: 10%;
-      text-align: left;
-    }
-    .project-right-top{
-      position: absolute;
-      top: 0;
-      right: 0;
-      border: 0;
-      width: 100%;
-    }
-    .site-logo{
-      width: 30%;
-      min-width: 165px;
-    }
-    .project-title{
-      padding-bottom: 0;
-      padding-top: 210px;
-      padding-right: 10%;
-      font-size: 36px;
-      text-align: right;
-      box-sizing: border-box;
-    }
-    .project-right{
-      border: 0;
-      word-break: keep-all;
-    }
-    .project-right-bottom{
-      padding: 40px 10%;
-    }
-  }
-  @media (max-width:768px) {
-    .site-logo-wrap{
-      text-align: center;
-      padding-left: 0;
-    }
-    .title-wrap{
-      padding-left: 0;
-      padding-top: 70px;
-    }
-    .project-left{
-      padding-bottom: 120px;
-    }
-    .project-right-top{
-    }
+    margin-bottom: 10px;
+    font-family: 'Noto Sans KR', sans-serif;
 
-    .project-title{
-      padding: 0;
-      text-align: center;
-      padding-top: 255px;
-      font-size: 20px;
-    }
-    .project-right-bottom{
-      font-size: 14px;
-    }
-    .details>h4{
-      font-size: 16px;
-    }
+  }
+  .disc-wrap p{
+    line-height: 22px;
+  }
+
+  .btns-wrap {
+    text-align: center;
+    margin-top: 40px;
+    display: none;
+    transition: all 0.3s;
+  }
+
+  .btns-wrap button {
+    outline: none;
+    border: 0;
+    background-color: #fff;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+
+  .btns-wrap button:last-child {
+    margin: 0;
+  }
+
+  .btns-wrap button img {
+    width: 30px;
+  }
+  .project-con-wrapper:hover{
+    transform: translateY(-5px);
+    box-shadow: 0px 0px 10px rgb(93, 93, 93);
+  }
+  .project-con-wrapper:hover .con-wrap .disc-wrap{
+    display: none;
+  }
+  .project-con-wrapper:hover .con-wrap .btns-wrap{
+    display: block;
   }
 </style>
--- src/components/ProjectSec.vue
+++ src/components/ProjectSec.vue
@@ -1,181 +1,137 @@
 <template>
-  <div class="wrap">
-    <div class="project-swiper-wrap">
-      <swiper :navigation="{
-        nextEl: '.next-btn',
-    prevEl: '.prev-btn',
-      }" :slides-per-view="1" :modules="modules" @swiper="onSwiper" @slideChange="onSlideChange">
-        <swiper-slide v-for="(item,index) in projectDetails" :key="item">
-          <project-con :logoUrl='item.logo' :title="item.tit" :disc="item.dis" :devtime="item.devt" :job="item.jobs"
-            :contr="item.cont" :stack="item.stak" :githubUrl="item.github" :demoUrl="item.demo" :total="projectDetails.length" 
-            :current="index+1" />
-        </swiper-slide>
-        <div class="prev-btn">
-          <font-awesome-icon icon="fa-solid fa-arrow-left" />
+  <div class="wrapper">
+    <div class="holder">
+      <div class="title-wrap">
+        <h1>PROJECT</h1>
+      </div>
+      <div class=" project-wrap renewal-project-wrap">
+        <h2>renewal</h2>
+        <div class="renewal-con">
+          <project-con/>  
+          <project-con/>  
+          <project-con/>  
         </div>
-        <div class="next-btn">
-          <font-awesome-icon icon="fa-solid fa-arrow-right" />
+      </div>
+      <div class=" project-wrap clone-wrap">
+        <h2>web clone</h2>
+        <div class="renewal-con">
+          <project-con/>  
+          <project-con/>  
         </div>
-      </swiper>
+      </div>
+      <div class=" project-wrap toy-project-wrap">
+        <h2>toy project</h2>
+        <div class="renewal-con">
+          <project-con/>  
+          <project-con/>  
+          <project-con/>  
+        </div>
+      </div>
 
     </div>
   </div>
 </template>
 <script>
-  import {
-    onMounted
-  } from "vue";
-
-  import ProjectCon from './ProjectCon.vue';
-
-  import {
-    Swiper,
-    SwiperSlide,
-  } from 'swiper/vue';
-  import {
-    Navigation
-  } from "swiper";
-  import 'swiper/css';
-  import 'swiper/css/navigation'
-  export default {
-    components: {
-      ProjectCon,
-      Swiper,
-      SwiperSlide,
-    },
-    setup() {
-      const onSwiper = () => {};
-      const onSlideChange = () => {};
-      const projectDetails = [{
-          logo: require(`@/assets/imgs/KWAlogo.png`),
+import ProjectCon from './ProjectCon.vue';
+export default {
+  components: { ProjectCon },
+  
+}
+const projectDetails = [{
+          logo: require(`@/assets/imgs/kwa-tit.png`),
           tit: '한국 폐기물 협회',
-          dis: `한국 폐기물협회 웹사이트의 리뉴얼을 진행했습니다. 
-디자인에도 문제가 많고, 테이블을 사용하여 웹 사이트가 만들어진 것, 
-반응형이 적용이 안 된 것을 보고 구조적으로도 문제가 많다고 생각했습니다.`,
-          devt: '2022 11월 초 ~ 12월 중순',
-          jobs: '1인 개발',
-          cont: '100% / 디자인, 마크업, 동적요소, 서버 구축, 데이터베이스 연결',
           stak: 'HTML, CSS, Javascript, Node.js, express.js, ejs, MySQL',
           github: 'https://github.com/walkingcabbage/KWA2',
-          demo: 'https://kwa-walkingcabbage.koyeb.app/'
+          demo: 'https://kwa-walkingcabbage.koyeb.app/',
+          mockup: true,
         },
         {
-          logo: require(`@/assets/imgs/seomunlogo.png`),
+          logo: require(`@/assets/imgs/seomun-tit.png`),
           tit: '서문시장 야시장',
-          dis: `서문시장 야시장 웹사이트의 리뉴얼을 진행하였습니다.
-매우 번잡한 콘텐츠 배치와 반응형 미지원을 고치는 데에 중점을 두었습니다. `,
-          devt: '2022 10월 초 ~ 11월 초 ',
-          jobs: '1인 개발',
-          cont: '100% / 디자인, 마크업, 동적요소, 서버 구축, 데이터베이스 연결',
           stak: 'HTML, CSS, Javascript, Node.js, express.js, ejs, MySQL',
           github: 'https://github.com/walkingcabbage/seomun',
-          demo: 'https://seomun-walkingcabbage.koyeb.app/'
+          demo: 'https://seomun-walkingcabbage.koyeb.app/',
+          mockup: true,
         },
         {
-          logo: require(`@/assets/imgs/wonjulogo.png`),
+          logo: require(`@/assets/imgs/wonju-tit.png`),
           tit: '원주 곤충마을 박물관',
-          dis: `원주 곤충마을 박물관 웹사이트의 리뉴얼을 진행하였습니다.
-마찬가지로 구식 디자인, 테이블과 이미지의 무분별한 사용, 반응형 미적용 등
-문제점이 많았습니다. 아예 새롭게 만들자는 생각으로 진행하였습니다.`,
-          devt: '2022 11월 초 ~ 12월 말',
-          jobs: '팀 프로젝트 / PM, 디자인, 퍼블리싱',
-          cont: '50% / 메인페이지 마크업, 서브페이지 마크업, 동적요소, 서버구축, 데이터베이스 연결',
           stak: 'HTML, CSS, Javascript, Node.js, express.js, ejs, MySQL, git',
           github: 'https://github.com/walkingcabbage/wonju',
-          demo: 'https://wonju-walkingcabbage.koyeb.app/'
+          demo: 'https://wonju-walkingcabbage.koyeb.app/',
+          mockup: true,
         },
-        {
-          logo: require(`@/assets/imgs/richmondlogo.png`),
-          tit: '더 리치먼드',
-          dis: `더 리치먼드 웹사이트 클론코딩을 진행하였습니다.
-반응형, 적절한 동적요소, 웹 표준 등 제 능력에 도움이 될 수 있다고 생각하였습니다.`,
-          devt: '2022 10월 초 ~ 10월 중순',
-          jobs: '1인 개발',
-          cont: '100% / 디자인, 마크업, 동적요소',
-          stak: 'HTML, CSS, Javascript',
-          github: 'https://github.com/walkingcabbage/therichmond_clone',
-          demo: 'https://walkingcabbage.github.io/therichmond_clone/'
-        },
-        {
-          logo: require(`@/assets/imgs/shouselogo.png`),
-          tit: '싱그러운 집',
-          dis: `싱그러운 집 웹사이트 클론코딩을 진행하였습니다.
-원본 웹사이트와는 다르게 flex를 이용하여 요소들을 배치하였고
-swiper 라이브러리를 사용할 수 있는 경험이 되었습니다.`,
-          devt: '2022 10월 중순 ~ 11월 초 ',
-          jobs: '1인 개발',
-          cont: '100% / 디자인, 마크업, 동적요소',
-          stak: 'HTML, CSS, Javascript',
-          github: 'https://github.com/walkingcabbage/shouse_clone',
-          demo: 'https://walkingcabbage.github.io/shouse_clone/'
-        },
+        
 
       ]
 
-      onMounted(() => {
+const cloneDetails = [
+        {
+          logo: require(`@/assets/imgs/richmond-tit.png`),
+          tit: '더 리치먼드',
+          stak: 'HTML, CSS, Javascript',
+          github: 'https://github.com/walkingcabbage/therichmond_clone',
+          demo: 'https://walkingcabbage.github.io/therichmond_clone/',
+          mockup: false
+        },
+        {
+          logo: require(`@/assets/imgs/shouse-tit.png`),
+          tit: '싱그러운 집',
+          stak: 'HTML, CSS, Javascript',
+          github: 'https://github.com/walkingcabbage/shouse_clone',
+          demo: 'https://walkingcabbage.github.io/shouse_clone/',
+          mockup: false,
+        },
 
-      })
-      return {
-        onSwiper,
-        onSlideChange,
-        projectDetails,
-        modules: [Navigation],
-      }
-    }
-  }
+      ]
+const toyDetails = [
+        {
+          logo: require(`@/assets/imgs/goodsleep-tit.png`),
+          tit: 'Good Sleep',
+          stak: 'HTML, CSS, Javascript, React.js, TypeScript',
+          github: 'https://github.com/walkingcabbage/sleepy-calc',
+          demo: 'https://walkingcabbage.github.io/sleepy-calc/',
+          mockup: false
+        },
+        {
+          logo: require(`@/assets/imgs/khstype-tit.png`),
+          tit: '장규한의 취향은',
+          stak: 'HTML, CSS, Javascript, React.js',
+          github: 'https://github.com/walkingcabbage/khs-type',
+          demo: 'https://walkingcabbage.github.io/khs-type/',
+          mockup: false,
+        },
+        {
+          logo: require(`@/assets/imgs/howmanydays-tit.png`),
+          tit: '얼마나 지났을까',
+          stak: 'HTML, CSS, Javascript, React.js',
+          github: 'https://github.com/walkingcabbage/how-many-days',
+          demo: 'https://walkingcabbage.github.io/how-many-days/',
+          mockup: false,
+        },
+
+      ]
 </script>
-<style scoped>
-  div.wrap {
-    width: 100%;
-    /* height: 100vh; */
-    min-height: 900px;
-    max-height: 1000px;
-    background-color: #000;
-  }
 
-  .project-swiper-wrap {
-    height: 100%;
+<style lang="css">
+  .wrapper{
+    padding: 30px 0;
+    background-color: #fff;
   }
-
-  .swiper {
-    height: 100%;
+  .title-wrap{
+    padding-bottom: 50px;
   }
-
-  .prev-btn {
-    z-index: 3;
-    position: absolute;
-    color: #fff;
-    font-size: 60px;
-    top: 50%;
-    left: 1%;
-    transform: translateY(-50%);
-    cursor: pointer;
-    transition: font-size 0.3s;
+  .project-wrap{
+    margin-bottom: 40px;
   }
-
-  .prev-btn:hover,
-  .next-btn:hover {
-    font-size: 70px;
+  .project-wrap >h2{
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
   }
-
-  .next-btn {
-    z-index: 3;
-    position: absolute;
-    color: #fff;
-    font-size: 60px;
-    top: 50%;
-    right: 1%;
-    transform: translateY(-50%);
-    cursor: pointer;
-    transition: font-size 0.3s;
-  }
-  @media (max-width:1400px) {
-    .prev-btn,.next-btn{
-      font-size: 36px;
-    }
-    .prev-btn:hover,
-  .next-btn:hover {
-    font-size: 40px;
-  }
+  .renewal-con{
+    display: flex;
+    justify-content: center;
+    gap: 20px;
   }
 </style>
